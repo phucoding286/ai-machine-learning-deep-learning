ee2dc6668198d55eb3df463abd4e79d82577c953
phucoding286 <phucoding286@gmail.com>
2024-07-26 13:24:32 +0700

    Update bag_of_word.py

--- bag_of_word.py
+++ bag_of_word.py
@@ -1,30 +1,53 @@
-class BagOfWord:
-    def __init__(self, batch_data):
-        self.batch_data = batch_data
-        self.max_len = 0
-    
-    # hàm đếm số lần xuất hiện của từ
-    def count(self, sentence):
-        bag = []
-        for char in list(sentence): 
-            appeer_times = 0
-            for char_count in list(sentence): appeer_times += 1 if char in char_count else 0
-            bag.append(appeer_times - 1 if appeer_times > 1 else 0)
-        return bag
-    
-    def fit(self, bag_txt):
-        # tạo lô mã hóa với cách điếm số lần xuất hiện của từ
-        batch_bag = []
-        for sentence in bag_txt:
-            count_out = self.count(sentence)
-            batch_bag.append(count_out)
-            if len(count_out) > self.max_len:
-                self.max_len = len(count_out)
-        # chuẩn hóa độ dài của văn bản mã hóa
-        for i in range(len(batch_bag)):
-            if len(batch_bag[i]) < self.max_len: 
-                batch_bag[i] = batch_bag[i] + [0 for _ in range(len(batch_bag[i]), self.max_len)]
-            else:
-                batch_bag[i] = batch_bag[i][:self.max_len]
+import numpy as np
 
-        return np.array(batch_bag)
+class BagOfWord:
+    def __init__(self):
+        self.maxlen = 0
+
+    def counter_tranform(self, x):
+        sent_enc = []
+        for char in list(x):
+            appeer_times = 0
+            for count in list(x):
+                if char == count:
+                    appeer_times += 1
+            sent_enc.append(appeer_times - 1)
+            if len(sent_enc) > self.maxlen:
+                self.maxlen = len(sent_enc)
+        return sent_enc
+    
+    def fit_transform(self, x_batch):
+        batch = []
+        for sentence in x_batch:
+            enc_op = self.counter_tranform(sentence)
+            batch.append(enc_op)
+        for i in range(len(batch)):
+            if len(batch[i]) < self.maxlen:
+                for _ in range(len(batch[i]), self.maxlen):
+                    batch[i].append(0)
+            else:
+                batch[i] = batch[i][:self.maxlen]
+        return np.array(batch)
+    
+    def counter(self, x):
+        sent_enc = []
+        for char in list(x):
+            appeer_times = 0
+            for count in list(x):
+                if char == count:
+                    appeer_times += 1
+            sent_enc.append(appeer_times - 1)
+        return sent_enc
+    
+    def fit(self, x):
+        batch = []
+        for sentence in x:
+            enc_op = self.counter(sentence)
+            batch.append(enc_op)
+        for i in range(len(batch)):
+            if len(batch[i]) < self.maxlen:
+                for _ in range(len(batch[i]), self.maxlen):
+                    batch[i].append(0)
+            else:
+                batch[i] = batch[i][:self.maxlen]
+        return np.array(batch)
